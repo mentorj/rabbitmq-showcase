5b4afdb771db98cd36559e6882a3b43a6af5765c
deadbrain <jerome@javaxpert.com>
2019-11-03 00:02:43 +0100

    improves thread usage - not yet perfect

--- src/test/java/com/javaxpert/demos/rabbitmq_showcase/MultipleConsumersTest.java
+++ src/test/java/com/javaxpert/demos/rabbitmq_showcase/MultipleConsumersTest.java
@@ -9,10 +9,9 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Tests suite showing how multiple consumers behave concurrently
@@ -34,18 +33,26 @@ public class MultipleConsumersTest {
             Channel channel = conn.createChannel();
             ExecutorService executor = Executors.newFixedThreadPool(5);
             Runnable prepareQueue = () -> {
+
                 try {
                     AMQP.Exchange.DeclareOk declareExchange = channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT, true);
                     AMQP.Queue.DeclareOk declareQueue = channel.queueDeclare(QUEUE_NAME, true, false, false, null);
                     AMQP.Queue.BindOk bindResult = channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
                     logger.info("Queue bound to exchange with result = " + bindResult.toString());
+                    channel.queuePurge(QUEUE_NAME);
+                    for(int i =0;i<MAX_MESSAGES;i++){
+                        String msg = "Msg number = " + i;
+                        channel.basicPublish(EXCHANGE_NAME,ROUTING_KEY,null,msg.getBytes());
+                    }
+                    logger.debug("prepare is over now..Messages sent");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
             };
-            prepareQueue.run();
+            Future prepare_result = executor.submit(prepareQueue);
             Thread.sleep(1000);
+            logger.info("Job finished ? = "+ prepare_result.isDone() + "Queue bound & messages published");
         } catch (TimeoutException | InterruptedException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -53,37 +60,66 @@ public class MultipleConsumersTest {
         }
     }
 
-        @Test(description = "show 1 consumer with prefetch set to 100 and 1 other with prefetch set to 0",enabled = true)
-    public void testWith1ProducerAnd2ConsumersSameCpu(){
+    @Test(description = "basic consumer 1 thread with 1 supervisor",enabled = true)
+    public void testWith1SingleConsumer(){
         ConnectionFactory factory = new ConnectionFactory();
         Connection conn;
         try {
             conn = factory.newConnection();
             Channel channel = conn.createChannel();
             ExecutorService executor = Executors.newFixedThreadPool(5);
-            Thread.sleep(2000);
-//            logger.info("now start consumers and producers");
-//            Runnable publisher = () -> {
-//                for(int i =0 ; i< MAX_MESSAGES;i++){
-//
-//                    try {
-//                        Thread.sleep(2);
-//                        channel.basicPublish(EXCHANGE_NAME,ROUTING_KEY,null,("Message number"+ i).getBytes());
-//                        if(i%100==0){
-//                            logger.debug("Messages ready to be delivered on queue: " + QUEUE_NAME+ " =" + channel.messageCount(QUEUE_NAME));
-//                        }
-//                    } catch (IOException | InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            };
-//            executor.submit(publisher);
-            Worker large = new Worker("largeCPU",50,0,QUEUE_NAME,channel,executor);
-            Worker small = new Worker("smallCPU",5,0,QUEUE_NAME,channel,executor);
+            //Thread.sleep(2000);
+            AtomicInteger counter = new AtomicInteger(0);
+            Runnable consumer = () -> {
+                logger.info("starting consumer...");
+                while(true){
+                    try {
+                        Thread.sleep(5);
+                        channel.basicConsume(QUEUE_NAME, (s, delivery) ->
+                                {
+                                    if (counter.get() % 10000 == 0) {
+                                        logger.debug("Received message = " + new String(delivery.getBody()));
+                                    }
+                                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                                    counter.getAndAdd(1);
+                                }
+                                ,
+                                s -> {
+                                    logger.warn("received cancelcallback");
+                                }
+                        );
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            Future consumer_result  = executor.submit(consumer);
+            Runnable supervisor = () -> {
+                logger.info("Starting supervisor");
+                while(true) {
+                    try {
+                        Thread.sleep(25);
+                        if (counter.get() == MAX_MESSAGES) {
+                            logger.info("Job seems to be finished.. Halting threads.\n Future status :" + consumer_result.isDone());
+                            consumer_result.cancel(true);
+                        } else {
+                            if (counter.get() > 50000) {
+                                logger.debug("Process still busy");
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
 
-            executor.awaitTermination(20, TimeUnit.SECONDS);
-            executor.shutdown();
-            logger.info("Result is : large consumed = "+ large.getMessagesProcessed() + " Small consumed : "+ small.getMessagesProcessed());
+            Future supervisor_result = executor.submit(supervisor);
+            executor.awaitTermination(10,TimeUnit.SECONDS);
+            if(consumer_result.isCancelled()){
+                logger.info("Consumer cancelled by supervisor");
+                Assert.assertTrue(true);
+            }
+            logger.info("Test finished");
             channel.close();
             conn.close();
         } catch (Exception e) {
@@ -91,25 +127,85 @@ public class MultipleConsumersTest {
             logger.warn("Error occured udring test",e);
             Assert.fail("Error during test");
         }
-
     }
 
-    @Test(enabled = false)
-    public void simpleTest(){
+    @Test(description = "basic consumer 1 thread with 1 supervisor prefetch set",enabled = true)
+    public void testWith1ConsumerPrefetch(){
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection conn;
         try {
-            Connection conn = new ConnectionFactory().newConnection();
+            conn = factory.newConnection();
             Channel channel = conn.createChannel();
-            try {
-                channel.basicConsume(QUEUE_NAME,(s, delivery) -> logger.debug("received message" + delivery.getBody())
-                        ,
-                        s -> logger.debug("cancel callback")
+            channel.basicQos(100);
+            ExecutorService executor = Executors.newFixedThreadPool(5);
+            //Thread.sleep(2000);
+            AtomicInteger counter = new AtomicInteger(0);
+            double start = System.nanoTime();
+            AtomicLong stop = new AtomicLong(0);
+            Runnable consumer = () -> {
+                logger.info("starting consumer...");
+                while(true){
+                    try {
+                        Thread.sleep(5);
+                        channel.basicConsume(QUEUE_NAME, (s, delivery) ->
+                                {
+                                    if (counter.get() % 10000 == 0) {
+                                        logger.debug("Received message = " + new String(delivery.getBody()));
+                                    }
+                                    if(counter.get()==MAX_MESSAGES){
+                                        stop.addAndGet(System.nanoTime());
+                                    }
+                                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                                    counter.getAndAdd(1);
+                                }
+                                ,
+                                s -> {
+                                    logger.warn("received cancelcallback");
+                                }
                         );
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            Future consumer_result  = executor.submit(consumer);
+            Runnable supervisor = () -> {
+                logger.info("Starting supervisor");
+                while(true) {
+                    try {
+                        Thread.sleep(25);
+                        if (counter.get() == MAX_MESSAGES) {
+                            logger.info("Job seems to be finished.. Halting threads.\n Future status :" + consumer_result.isDone());
+                            consumer_result.cancel(true);
+                        } else {
+                            if (counter.get() > 50000) {
+                                logger.debug("Process still busy");
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            Future supervisor_result = executor.submit(supervisor);
+            executor.awaitTermination(10,TimeUnit.SECONDS);
+            if(consumer_result.isCancelled()){
+                logger.info("Consumer cancelled by supervisor");
+                logger.info("elapsed time for test =" + ((stop.doubleValue() - start)/1000));
+
+                Assert.assertTrue(true);
+            }
+            logger.info("Test finished");
+
         }
+        catch (Exception e) {
+            e.printStackTrace();
+            logger.warn("Error occured udring test",e);
+            Assert.fail("Error during test");
+        }
+
     }
+
+
 }
--- src/test/java/com/javaxpert/demos/rabbitmq_showcase/Worker.java
+++ src/test/java/com/javaxpert/demos/rabbitmq_showcase/Worker.java
@@ -67,7 +67,9 @@ public class Worker extends DefaultConsumer {
 
     @Override
     public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-
+        super.handleDelivery(consumerTag, envelope, properties, body);
+        logger.info("handleDelivery invoked");
         threadPool.submit(new MessagerHandlerTask(envelope.getDeliveryTag(),delay,channel,this));
+
     }
 }
